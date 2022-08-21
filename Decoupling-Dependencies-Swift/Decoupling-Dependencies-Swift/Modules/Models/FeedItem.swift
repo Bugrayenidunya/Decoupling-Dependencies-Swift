2cf7393c478ccd7803ab2d474b36b6c015b67c0c
Enes Buğra Yenidünya <bugrayenidunya@gmail.com>
2022-08-21 20:51:28 +0300

    Add FeedItem model

---
+++ Decoupling-Dependencies-Swift/Decoupling-Dependencies-Swift/Modules/Models/FeedItem.swift
@@ -0,0 +1,14 @@
+//
+//  FeedItem.swift
+//  Decoupling-Dependencies-Swift
+//
+//  Created by Enes Buğra Yenidünya on 20.08.2022.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID = .init()
+    let name: String
+    let imageUrl: String
+}
